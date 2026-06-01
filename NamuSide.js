d9b8c3ab92a1d226c1d41595c6fe01bec8902555
loveseoul9900 <justlondonit@gmail.com>
2026-06-01 12:50:16 +0900

    Update NamuSide.js

--- NamuSide.js
+++ NamuSide.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NamuSide
 // @namespace    http://tampermonkey.net/
-// @version      2025-06-24
+// @version      2026-06-01
 // @description        NamuSide blocks the side Navagation Bar on NamuWiki.
 // @description:ko     NamuSide는 나무위키의 사이드바를 제거합니다
 // @author       loveseoul9900
@@ -17,4 +17,4 @@
 // @updateURL https://update.greasyfork.org/scripts/497290/NamuSide.meta.js
 // ==/UserScript==
 
-function a(){var b=document.getElementsByTagName("ul"),c=[],d=[],e=[];for(var f=0;f<b.length;f++){var g=b[f],h=g.getElementsByTagName("li").length;if(h===10){var i=g.parentNode.parentNode;c.push(i);}else if(h===5){var j=g.parentNode.parentNode;d.push(j);var k=g.parentNode.parentNode.parentNode;e.push(k);}}var l=[];for(var m=0;m<c.length;m++){var n=c[m];if(d.indexOf(n)!==-1){l.push(n);}}if(l.length>0){l[0].remove();}else{var o=Array.from(new Set([...d,...e])),p=c.filter(q=>o.includes(q));if(p.length>0){p[0].remove();}}}const r=document.body,s1={childList:true,subtree:true},t1=function(u,v){for(const w of u){if(w.type==='childList'){a();}}},x=new MutationObserver(t1);x.observe(r,s1);a();
+function a(){var b=document.getElementsByTagName("ul");for(var c=0;c<b.length;c++){var d=b[c],e=Array.from(d.children).filter(f=>f.tagName==='LI').length;if(e===10){var g=d.parentNode.parentNode;if(g&&g.tagName==='DIV'){g.remove();}}}}const h=document.body,i={childList:true,subtree:true},j=function(k,l){for(const m of k){if(m.type==='childList'){a();}}},n=new MutationObserver(j);n.observe(h,i);setInterval(a,3000);a();
