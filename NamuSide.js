b16033942a692d35259b75f4b0aae810e5ee2c22
loveseoul9900 <justlondonit@gmail.com>
2026-06-13 04:43:28 +0900

    Update NamuSide.js

--- NamuSide.js
+++ NamuSide.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NamuSide
 // @namespace    http://tampermonkey.net/
-// @version      2026-06-13-3
+// @version      2026-06-13-4
 // @description        NamuSide blocks the side Navagation Bar on NamuWiki.
 // @description:ko     NamuSide는 나무위키의 사이드바를 제거합니다
 // @author       loveseoul9900
@@ -12,9 +12,9 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @grant        unsafeWindow
-// @run-at       document-end
+// @run-at       document-idle
 // @downloadURL https://update.greasyfork.org/scripts/497290/NamuSide.user.js
 // @updateURL https://update.greasyfork.org/scripts/497290/NamuSide.meta.js
 // ==/UserScript==
 
-var a1=false;function b1(){if(a1)return;var c1=document.getElementsByTagName("ul");for(var d1=c1.length-1;d1>=0;d1--){var e1=c1[d1];var f1=Array.from(e1.children).filter(function(g1){var h1=g1.tagName==='LI';var i1=g1.querySelectorAll('a').length===1;return h1&&i1;}).length;if(f1===10){var j1=e1.parentNode.parentNode;if(j1&&j1.tagName==='DIV'){j1.remove();console.log("Success");a1=true;break;}}}}const k1=document.body;const l1={childList:true,subtree:true};const m1=function(n1,o1){if(a1){o1.disconnect();return;}for(const p1 of n1){if(p1.type==='childList'){b1();}}};const q1=new MutationObserver(m1);q1.observe(k1,l1);const r1=setInterval(function(){if(a1){clearInterval(r1);return;}b1();},3000);b1();
+if(!window.z1){window.z1=false;setTimeout(function(){function a1(){if(window.z1)return;var b1=document.getElementsByTagName("ul");for(var c1=b1.length-1;c1>=0;c1--){var d1=b1[c1];var e1=Array.from(d1.children).filter(function(f1){return f1.tagName==='LI'}).length;if(e1===10){var g1=d1.parentNode.parentNode;if(g1&&g1.tagName==='DIV'){g1.remove();window.z1=true;break}}}}const r1=document.body;const s1={childList:true,subtree:true};const t1=function(u1,v1){if(window.z1){x1.disconnect();return}for(const w1 of u1){if(w1.type==='childList'){a1()}}};const x1=new MutationObserver(t1);x1.observe(r1,s1);const m1=setInterval(function(){if(window.z1){clearInterval(m1);return}a1()},3000);a1()},2000);}
